5e7f6c02492c646b8eafb6175996e060c08c76dd
Juan Villacorta <juanvillacortac@gmail.com>
2022-11-30 20:07:10 -0400

    Logo

--- apps/web/src/lib/Logo.svelte
+++ apps/web/src/lib/Logo.svelte
@@ -9,18 +9,30 @@
 
 <Viewport class="flex space-x-4 items-center" --a-t="600ms" once>
   <div class="flex space-x-2 items-center">
-    <div class="flex" class:anim style:--anim-s="0.1" style:--anim-d="100ms">
-      {#if place}
+    {#if place}
+      <div class="flex" class:anim style:--anim-s="0.1" style:--anim-d="100ms">
+        <!-- {#if place}
         <Map24 class="flex" />
-      {:else}
+      {:else} -->
         <MapBoundary24 class="flex" />
-      {/if}
-    </div>
-    <span
-      class="font-bold font-title text-sm text-dark-900 uppercase anim dark:text-gray-100"
-      style:--anim-x="-0.1rem"
-      style:--anim-d="300ms"
-      class:anim>{place?.name || 'Maneiro es Turismo'}</span
-    >
+        <!-- {/if} -->
+      </div>
+      <span
+        class="font-bold font-title text-sm text-dark-900 uppercase anim dark:text-gray-100"
+        style:--anim-x="-0.1rem"
+        style:--anim-d="300ms"
+        class:anim>{place?.name || 'Maneiro es Turismo'}</span
+      >
+    {:else}
+      <img
+        src="/images/logo_alt.svg"
+        alt="Maneiro es Turismo"
+        class="h-38px w-auto"
+        class:anim
+        style:--anim-x="-0.1rem"
+        style:--anim-s="0.1"
+        style:--anim-d="100ms"
+      />
+    {/if}
   </div>
 </Viewport>
