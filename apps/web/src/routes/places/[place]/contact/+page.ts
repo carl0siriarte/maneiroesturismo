398ce35581460ce9e02d540bd1147ef32801debd
Juan Villacorta <juanvillacortac@gmail.com>
2022-12-04 16:53:10 -0400

    ...

---
+++ apps/web/src/routes/places/[place]/contact/+page.ts
@@ -0,0 +1,11 @@
+import { error } from '@sveltejs/kit'
+import type { PageLoad } from './$types'
+export const load: PageLoad = async ({ parent }) => {
+  const { contextData } = await parent()
+  if (contextData.layout == 'place') {
+    throw error(404, 'Página no encontrada')
+  }
+  return {
+    title: 'Miembros',
+  }
+}
